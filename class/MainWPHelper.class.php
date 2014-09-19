21a2053d84e0060a145d0b5cf52a9f270388efa8
mainwp <mainwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 19:48:13 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/mainwp-child/trunk@993527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class/MainWPHelper.class.php
+++ class/MainWPHelper.class.php
@@ -146,17 +146,24 @@ class MainWPHelper
                     $originalImgUrl = $imgUrl;
                 }
 
-                $downloadfile = MainWPHelper::uploadImage($originalImgUrl);
-                $localUrl = $downloadfile['url'];
-                $linkToReplaceWith = dirname($localUrl);
-                if ($hrefLink != '')
+                try
                 {
-                    $lnkToReplace = dirname($hrefLink);
+                    $downloadfile = MainWPHelper::uploadImage($originalImgUrl);
+                    $localUrl = $downloadfile['url'];
+                    $linkToReplaceWith = dirname($localUrl);
+                    if ($hrefLink != '')
+                    {
+                        $lnkToReplace = dirname($hrefLink);
+                        if ($lnkToReplace != 'http:' && $lnkToReplace != 'https:') $new_post['post_content'] = str_replace($lnkToReplace, $linkToReplaceWith, $new_post['post_content']);
+                    }
+
+                    $lnkToReplace = dirname($imgUrl);
                     if ($lnkToReplace != 'http:' && $lnkToReplace != 'https:') $new_post['post_content'] = str_replace($lnkToReplace, $linkToReplaceWith, $new_post['post_content']);
                 }
+                catch (Exception $e)
+                {
 
-                $lnkToReplace = dirname($imgUrl);
-                if ($lnkToReplace != 'http:' && $lnkToReplace != 'https:') $new_post['post_content'] = str_replace($lnkToReplace, $linkToReplaceWith, $new_post['post_content']);
+                }
             }
         }
         

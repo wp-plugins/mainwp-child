2c2fcbc6f2b2877523ad62bbeccc000290b5261a
mainwp <mainwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 18:52:52 +0000

    * Added: Support for the display Favicon for child sites feature * Added: Support for upcoming extension * Fixed: Plugin conflicts with Wordpress SEO by Yoast and Backupbuddy
    git-svn-id: https://plugins.svn.wordpress.org/mainwp-child/trunk@1128945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class/MainWPChild.class.php
+++ class/MainWPChild.class.php
@@ -11,7 +11,7 @@ include_once(ABSPATH . '/wp-admin/includes/plugin.php');
 
 class MainWPChild
 {
-    private $version = '2.0.9';
+    private $version = '2.0.10';
     private $update_version = '1.0';
 
     private $callableFunctions = array(
@@ -65,7 +65,9 @@ class MainWPChild
         'wordfence' => 'wordfence',
         'delete_backup' => 'delete_backup',
         'update_values' => 'update_values',
-        'ithemes' => 'ithemes'
+        'ithemes' => 'ithemes',
+        'multistores' => 'multistores',
+        'updraftplus' => 'updraftplus'
     );
 
     private $FTP_ERROR = 'Failed, please add FTP details for automatic upgrades.';
@@ -398,6 +400,8 @@ class MainWPChild
 
     function update_htaccess($hard = false)
     {
+        if (defined('DOING_CRON') && DOING_CRON) return;
+
         if ((get_option('mainwp_child_pluginDir') == 'hidden') && ($hard || (get_option('mainwp_child_htaccess_set') != 'yes')))
         {
             include_once(ABSPATH . '/wp-admin/includes/misc.php');
@@ -677,8 +681,18 @@ class MainWPChild
             $open_location = isset($_REQUEST['open_location']) ? $_REQUEST['open_location'] : '';  
             if (!empty($open_location)) {
                 $open_location = base64_decode ($open_location);
-                if (strpos($open_location, "nonce=child_temp_nonce") !== false)
-                    $open_location = str_replace ("nonce=child_temp_nonce", "nonce=" . wp_create_nonce('wp-ajax'), $open_location);
+                $_vars = MainWPHelper::parse_query($open_location);
+                $_path = parse_url($open_location, PHP_URL_PATH);
+
+                if (isset($_vars['_mwpNoneName']) && isset($_vars['_mwpNoneValue'])) {
+                    $_vars[$_vars['_mwpNoneName']] = wp_create_nonce($_vars['_mwpNoneValue']);
+                    unset($_vars['_mwpNoneName']);
+                    unset($_vars['_mwpNoneValue']);
+                    $open_location = "/wp-admin/" . $_path . "?" . http_build_query($_vars);
+                } else {
+                    if (strpos($open_location, "nonce=child_temp_nonce") !== false)
+                        $open_location = str_replace ("nonce=child_temp_nonce", "nonce=" . wp_create_nonce('wp-ajax'), $open_location);
+                }
                 wp_redirect(site_url() . $open_location);
                 exit();
             }
@@ -775,6 +789,10 @@ class MainWPChild
         }
 
         new MainWPChildIThemesSecurity();
+
+        MainWPChildWooCommerceMultiStores::Instance()->init();
+        MainWPChildUpdraftplusBackups::Instance()->updraftplus_init();
+
         //Call the function required
         if (isset($_POST['function']) && isset($this->callableFunctions[$_POST['function']]))
         {
@@ -949,16 +967,20 @@ class MainWPChild
                     MainWPHelper::error($error);
                 }
             }
+
             if ($_POST['type'] == 'plugin' && isset($_POST['activatePlugin']) && $_POST['activatePlugin'] == 'yes')
             {
                 $path = $result['destination'];
+                $rslt = null;
+                wp_cache_set('plugins', array(), 'plugins');
                 foreach ($result['source_files'] as $srcFile)
                 {
+                    if (is_dir($path . $srcFile)) continue;
+
                     $thePlugin = get_plugin_data($path . $srcFile);
                     if ($thePlugin != null && $thePlugin != '' && $thePlugin['Name'] != '')
                     {
                         activate_plugin($path . $srcFile, '', false, true);
-                        break;
                     }
                 }
             }
@@ -1071,7 +1093,8 @@ class MainWPChild
      */
     function upgradePluginTheme()
     {
-        $wp_filesystem = $this->getWPFilesystem();
+        //Prevent disable/re-enable at upgrade
+        define('DOING_CRON', true);
 
         include_once(ABSPATH . '/wp-admin/includes/class-wp-upgrader.php');
 //        if (file_exists(ABSPATH . '/wp-admin/includes/deprecated.php')) include_once(ABSPATH . '/wp-admin/includes/deprecated.php');
@@ -2496,6 +2519,23 @@ class MainWPChild
             if (count($conflicts) > 0) $information['themeConflicts'] = $conflicts;
         }
 		
+        if (isset($_POST['othersData']))
+        {
+            $othersData = json_decode(stripslashes($_POST['othersData']), true);
+            if (!is_array($othersData))
+                $othersData = array();
+
+            do_action("mainwp-site-sync-others-data", $othersData);
+
+            if (isset($othersData['syncUpdraftData']) && $othersData['syncUpdraftData']) {
+                if (MainWPChildUpdraftplusBackups::isActivatedUpdraftplus()) {
+                    $information['syncUpdraftData'] =   MainWPChildUpdraftplusBackups::Instance()->syncData();
+                }
+            }
+        }
+
+        $information['faviIcon'] = $this->get_favicon();
+
         $last_post = wp_get_recent_posts(array( 'numberposts' => absint('1')));
         if (isset($last_post[0])) $last_post = $last_post[0];
         if (isset($last_post)) $information['last_post_gmt'] = strtotime($last_post['post_modified_gmt']);
@@ -2504,6 +2544,22 @@ class MainWPChild
         if ($exit) MainWPHelper::write($information);
 
         return $information;
+    }
+
+    function get_favicon() {
+        $url = site_url();
+        $request = wp_remote_get( $url, array('timeout' => 50));
+        $favi = "";
+        if (is_array($request) && isset($request['body'])) {
+            $preg_str = '/(<link\s+(?:[^\>]*)(?:rel="(?:shortcut\s+)?icon"\s*)(?:[^>]*)?href="([^"]+)"(?:[^>]*)?>)/is';
+            if (preg_match($preg_str, $request['body'], $matches))
+            {
+                $favi = $matches[2];
+            } else if (file_exists(ABSPATH . 'favicon.ico')) {
+                $favi = 'favicon.ico';
+            }
+        }
+        return $favi;
     }
 
     function scanDir($pDir, $pLvl)
@@ -4002,7 +4058,7 @@ class MainWPChild
                 MainWPHelper::update_option('heatMapsIndividualOverrideSetting', $override);             
                 MainWPHelper::update_option('heatMapsIndividualDisable', $disable);            
                 $this->update_htaccess(true);
-                }            
+            }
             MainWPHelper::write(array('result' => 'success'));
         }             
         MainWPHelper::write(array('result' => 'fail'));         
@@ -4018,6 +4074,14 @@ class MainWPChild
 
     function ithemes() {
         MainWPChildIThemesSecurity::Instance()->action();
+    }
+
+    function multistores() {
+        MainWPChildWooCommerceMultiStores::Instance()->action();
+    }
+
+    function updraftplus() {
+        MainWPChildUpdraftplusBackups::Instance()->action();
     }
 
     function delete_backup()
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Author URI: https://mainwp.com
 Plugin URI: https://mainwp.com
 Requires at least: 3.6
 Tested up to: 4.1
-Stable tag: 2.0.9.2
+Stable tag: 2.0.10
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -57,6 +57,11 @@ To see full documentation and FAQs please visit [MainWP Documentation](http://do
 10. The Extension Screen
 
 == Changelog ==
+
+= 2.0.10 =
+* Added: Support for the display Favicon for child sites feature
+* Added: Support for upcoming extension
+* Fixed: Plugin conflicts with Wordpress SEO by Yoast and Backupbuddy
 
 = 2.0.9.2 =
 * Fixed: Bug where SEO values are not being set for Boilerplate Pages and Posts
